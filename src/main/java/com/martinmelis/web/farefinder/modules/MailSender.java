9779cae848ee7c7fa4a312f1a3964e5a94a04ab9
Martin Melis <Martin Melis@DESKTOP-C2I41OU>
2017-12-23 22:15:02 +0100

    mail sender change

--- src/main/java/com/martinmelis/web/farefinder/modules/MailSender.java
+++ src/main/java/com/martinmelis/web/farefinder/modules/MailSender.java
@@ -22,18 +22,18 @@ public class MailSender {
 		final String password = "Patrolman486@1";
 
 		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
 
-		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		  });
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username,password);
+                    }
+                });
 
 		try {
 

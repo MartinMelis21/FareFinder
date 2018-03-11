9585abf751384e4e7736bf823f9a79bee3821f6b
Martin Melis <Martin Melis@DESKTOP-C2I41OU>
2018-03-11 17:12:28 +0100

    test

--- src/main/java/com/martinmelis/web/farefinder/servlet/Sandbox.java
+++ src/main/java/com/martinmelis/web/farefinder/servlet/Sandbox.java
@@ -115,8 +115,8 @@ public class Sandbox extends HttpServlet{
          option.setBinary(ffBinary);
          
          //System.setProperty("webdriver.gecko.driver",browserDriver.getAbsolutePath());
-         System.setProperty("webdriver.gecko.driver",geckodriver.getAbsolutePath());
-         WebDriver driver = new FirefoxDriver ();
+         //System.setProperty("webdriver.gecko.driver",geckodriver.getAbsolutePath());
+         WebDriver driver = new FirefoxDriver (option);
         //FirefoxDriver(option);
   		//comment the above 2 lines and uncomment below 2 lines to use Chrome
   		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
